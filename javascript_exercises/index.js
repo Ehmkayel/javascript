f5f2be1320d17ddb08a6b7b449df8213b74f1c51
lamidimorufat <lamidimorufat0@gmail.com>
2023-12-27 23:34:06 +0100

    done solving SuperSimpleDev Javascript level 1  exercises

---
+++ javascript_exercises/index.js
@@ -0,0 +1,46 @@
+// use alert(...); to display "Good morning!" in a popup
+alert("Good morning!");
+// display your name in a popup
+alert("My name is Lamidi Morufat Kajogbola")
+// using math, calculate 10+5 in the console; I will console the result rather than calculating it in the console
+
+const bigNumber = 10;
+const smallNumber = 5;
+const both = bigNumber + smallNumber;
+console.log(both)
+
+// calculate 20 - 5 in the console: I will console the result rather than calculating it in the console
+const first = 20;
+const second = 5;
+const result = first - second;
+console.log(result)
+
+// calculate 2 + 2 - 5 in the console; I will console the result rather than calculating it in the console
+const even = 2;
+const odd = 5;
+const total = even + even - odd;
+console.log(total)
+
+// use innerHtml to write good morning on the web
+document.body.innerHTML = "Good Morning!";
+// use innerHTML to write your name on the web
+document.body.innerHTML = "Lamidi Morufat Kajogbola";
+// you order a t-shirt for $10, socks for $8, and dinner plates for $20. use javascript tp calculate the total cost of your order 
+const tShirt = 10;
+const socks = 8;
+const dinnerPlates = 20;
+const totalCost = tShirt + socks + dinnerPlates;
+console.log(totalCost)
+
+// your bank account was $100, you spend $20 on lunch, $50 on dinner, and earn $200 from your job. calculate how much money you have
+const account = 100;
+const spend = 20;
+const dinner = 50;
+const earn = 200;
+const totalAmountLeft = earn + account - (spend + dinner);
+console.log(totalAmountLeft)
+
+// use innerHTML to make the webpage blank
+document.body.innerHTML = "";
+
+
---
+++ javascript_exercises/readme.md
@@ -0,0 +1,8 @@
+# This file will contain the questions to SuperSimpleDev exercises questions from his course javascript beginner to Pro
+
+# Lesson 1 Exercises:
+ - Use alert(...);to display "Good morning!" in a popup
+ - Display your name in a popup
+ - Using math, calculate 10 + 5 in the console
+ - Calculate 20 - 5 in the console
+ - Calculate 2 + 2 - 5 in the console
