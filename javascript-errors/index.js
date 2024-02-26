3b46c0ce213dec5c2434687ddf4a11f0c7669299
lamidimorufat <lamidimorufat0@gmail.com>
2024-02-26 23:33:33 +0100

    javascript errors

---
+++ javascript-errors/index.js
@@ -0,0 +1,25 @@
+// The try statement allows you to check whether a specific block of code contains an error or not 
+// while the catch statements allows you to display the error found in the try block
+
+try{
+    alert("The name is Ramadan");
+}
+catch (err){
+    console.log(err)
+}
+// the below is going to throw an error of papalert not defined
+try{
+    papalert("The name is Ramadan");
+}
+catch (err){
+    console.log(err)
+}
+
+// throw lets you create your custom error
+
+try{
+    throw Error("I love coding");
+}
+catch (err){
+    console.log(err);
+}
