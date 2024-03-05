159b75dd0172256345d1170f0a764e4de66b8d7c
lamidimorufat <lamidimorufat0@gmail.com>
2024-03-05 20:52:45 +0100

    data types

---
+++ Data types/index.js
@@ -0,0 +1,32 @@
+// We have 8 types of data types which includes:
+// Number
+// Strings
+// Boolean
+// Objects
+//  Symbols
+// Undefined
+// null
+// bigInt
+
+
+
+
+// The number type
+let number = 145; //integer
+number = 2.5; //floating point number
+alert("Ramadan" / 5); //NAN(Not a number)
+
+// The string type
+// there are three types of ways of defining a string; 
+// Double quotes "Ramadan"
+// Single quotes 'Ramadan'
+// Template strings `Ramadan`
+
+// example of template strings
+const profile = "Ramadan";
+const age = 29;
+const school = "University of Ilorin";
+// embedding a variable
+console.log(`My data are: ${profile} ${age} ${school}`);
+// embedding an expression
+alert(`The semester result is ${50 + 200}`)
