c054060372a672f7b32fea0f02fa8595473d3e91
lamidimorufat <lamidimorufat0@gmail.com>
2024-03-05 19:45:04 +0100

    created a variable folder for my variables study

---
+++ variables/index.js
@@ -0,0 +1,31 @@
+// Variable naming have limitations as variables can only be name like the below formats;
+// The first character must not start with a digit
+// The name of a variable must contain only letters, digit or underscore(_) and dollar symbol($)
+
+let message;
+message = "Hello Ramadan!";
+alert(message);
+
+// multiline variables
+const profile = "Ramadan";
+const age = 29;
+const school = "University of Ilorin";
+console.log(`My data are: ${profile} ${age} ${school}`);
+
+// Declare two variables: admin and name.
+// Assign the value "John" to name.
+// Copy the value from name to admin.
+// Show the value of admin using alert (must output “John”).
+
+// solution 
+let admin;
+let name;
+name = "john";
+admin = name;
+alert(admin)
+
+// Create a variable with the name of our planet. How would you name such a variable?
+// Create a variable to store the name of a current visitor to a website. How would you name that variable?
+
+const planet = "earth";
+const currentUser = "Ramadan";
