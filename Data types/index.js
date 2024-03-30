1e8dde3a83e9f50bd0562b125bc7c5cf222aa474
lamidimorufat <lamidimorufat0@gmail.com>
2024-03-30 22:15:41 +0100

    modified data types with a challenge

--- Data types/index.js
+++ Data types/index.js
@@ -30,3 +30,51 @@ const school = "University of Ilorin";
 console.log(`My data are: ${profile} ${age} ${school}`);
 // embedding an expression
 alert(`The semester result is ${50 + 200}`)
+
+//solution 5
+let bola = "emilia"
+let figure = 5;
+let kareem
+let ngozi = null;
+let bool = true;
+let z = 
+
+console.log(typeof bola);
+console.log(typeof number);
+console.log(typeof kareem)
+console.log(typeof ngozi)
+console.log(typeof bool)
+console.log(typeof z)
+
+//solution 6
+
+let value 
+let emily
+let numb
+let tell
+
+// solution 7
+const rama = "name";
+const num = -8.9;
+const date = "october";
+const fals = true;
+
+// solution 8
+const firstName = "Morufat"; 
+const lastName = "Lamidi";
+const maritalStatus = "single";
+const country = "Nigeria";
+
+
+// solution 9
+
+// let firstName = "Morufat", lastName = "Lamidi", maritalStatus = "single", country = "Nigeria", age = 25
+
+// solution 10
+let myAge = 25;
+let yourAge = 30;
+console.log("I am:", myAge);
+console.log("You are:", yourAge)
+
+
+
