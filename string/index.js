9d53583f0993b69823df41b8d320d3bcc301ae1f
lamidimorufat <lamidimorufat0@gmail.com>
2024-04-03 20:04:12 +0100

    string folder created

---
+++ string/index.js
@@ -0,0 +1,15 @@
+// strings can be created in three ways
+// - single quote ('')
+// - double quote ("")
+// - template strings (``)
+
+let profileName = 'Ramadan' //single quote
+profileName = "Risqoh" //double quote
+profileName = `Khayr` //template strings
+
+//we have different string methods
+let ramadan = "profile name"
+console.log(ramadan.toUpperCase())
+console.log(ramadan.length)
+console.log(ramadan.slice(3, 7))
+console.log(profileName.toLowerCase())
