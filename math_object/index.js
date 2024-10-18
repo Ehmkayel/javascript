f9df521f4916d75fc899299041acbffa470b471a
Ehmkayel <lamidimorufat0@gmail.com>
2024-10-19 00:43:02 +0100

    update: updated the string folder with template literals

---
+++ math_object/index.js
@@ -0,0 +1,23 @@
+const firstNumber = 200
+const secondNumber = 50
+let value;
+
+
+value = firstNumber + secondNumber
+value = firstNumber - secondNumber
+value = firstNumber * secondNumber
+value = firstNumber / secondNumber
+
+// Math Object
+value = Math.E
+value = Math.PI
+value = Math.ceil(3.6)
+value = Math.floor(3.6)
+value = Math.sqrt(64)
+value = Math.abs(-3)
+value = Math.pow(4, 5)
+value = Math.min(33, 10, 0, -1, 2)
+value = Math.max(33, 10, 0, -1, 2)
+value = Math.random() * 20
+value = Math.floor(Math.random() * 20)
+console.log(value)
--- string/index.js
+++ string/index.js
@@ -13,3 +13,47 @@ console.log(ramadan.toUpperCase())
 console.log(ramadan.length)
 console.log(ramadan.slice(3, 7))
 console.log(profileName.toLowerCase())
+
+const firstName = 'Ramadan'
+const lastName = 'Lamidi'
+let value = firstName + lastName
+value = firstName + ' ' + lastName
+
+// Escaping
+value = 'I can\'t Wait'
+
+// Concat
+value = firstName.concat(' ', lastName)
+
+// Change casing
+value = firstName.toLowerCase()
+value = firstName.toUpperCase()
+
+value = firstName.indexOf('d') 
+value = firstName.lastIndexOf('m')
+
+value = firstName.substring(0, 4)
+value = firstName.slice(0, 4)
+
+console.log(value)
+
+// Template Literals
+const name = 'Ramadan'
+const age = 30
+const job = 'Frontend Developer'
+const location = 'Kaduna'
+let html
+
+// html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + '</li><li> '+ job + '</li><li>City: '+ location + '</li></ul>'
+// document.body.innerHTML = html 
+
+html = `
+  <ul>
+    <li>Name: ${name}</li>
+    <li>Age: ${age}</li>
+    <li>Job: ${job}</li>
+    <li>Location: ${location}</li>
+    
+  </ul>
+`
+document.body.innerHTML = html 
