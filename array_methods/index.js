9a71af097d1009b4facac6f9c64f4161fd6829f2
Ehmkayel <lamidimorufat0@gmail.com>
2024-10-20 21:16:39 +0100

    updated the array-methods with my studies on arrays

--- array_methods/index.js
+++ array_methods/index.js
@@ -16,3 +16,24 @@ const mainNames = names.map((name) =>{
 })
 console.log(mainNames)
 
+const numbers = [35, 46, 34, 22]
+const numbers2 = new Array(22, 45, 0, 10)
+const fruit = ['apple', 'gauva', 'mango']
+const mixedValues = [45, 'hello', false, undefined, new Date(), null]
+let value 
+value = numbers.length
+value = Array.isArray(numbers)
+
+value = numbers[0]
+
+numbers[2] = 100
+
+value = numbers.indexOf(46)
+
+numbers.push(39)
+numbers.unshift(120)
+numbers.reverse()
+value = numbers.concat(numbers2)
+value = fruit.sort()
+console.log(numbers)
+console.log(value) 
