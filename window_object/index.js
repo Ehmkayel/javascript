97f77061e2166f93f12aab9a7b3102b70694d7b1
Ehmkayel <lamidimorufat0@gmail.com>
2024-12-05 11:08:29 +0100

    study-update: window object

---
+++ window_object/index.js
@@ -0,0 +1,27 @@
+window.alert('Hello World')
+
+const input = prompt()
+alert(input)
+
+if(confirm('Are you sure you want to delete')){
+    console.log('YES')
+}else {
+    console.log('NO')
+}
+
+let val;
+val = window.outerHeight
+val = window.outerWidth
+val = window.innerHeight
+val = window.innerWidth
+
+// scroll
+val = window.scrollY
+
+val = window.navigator
+val = window.navigator.userAgent
+val = window.navigator.language
+
+
+
+console.log(val)
